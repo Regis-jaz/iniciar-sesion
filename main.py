d6ab785e0a51671b3a65dbc0f2de5b87278694ea
REGIS <23308060610258@cetis61.edu.mx>
2026-03-13 10:35:58 -0500

    main.py

--- main.py
+++ main.py
@@ -1,45 +1,50 @@
 import flet as ft
 
 def main(page: ft.Page):
-    page.title = "inicio de sesion"
+    page.title = "Inicio de sesión"
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
-    
+
+    icono = ft.Icon(
+        icon=ft.Icons.AUTO_AWESOME,
+        color="pink200",
+        size=60
+    )
+
     titulo = ft.Text(
-            "Iniciar Sesion",
-            bgcolor="pink50",
-            size=30,
-            weight="bold", 
-        )
-    
-    
+        "Iniciar Sesión",
+        size=30,
+        weight="bold",
+        color="pink200"
+    )
 
     nombre = ft.TextField(
-            label="Nombre",
-            hint_text="",
-            border_color="pink200",
-            bgcolor="pink50",
-            color="white",
-            width=300
-        )
+        label="Nombre",
+        border_color="pink200",
+        width=300
+    )
 
-    conraseña = ft.TextField(
-            label="Contraseña",
-            password=True,
-            can_reveal_password=True,
-            border_color="pink200",
-            bgcolor="pink50",
-            color="white",
-            width=300
-        )
+    contrasena = ft.TextField(
+        label="Contraseña",
+        password=True,
+        can_reveal_password=True,
+        border_color="pink200",
+        width=300
+    )
 
-    
     boton = ft.Button(
-            content="Ingresar",
-            bgcolor="pink200",
-            color="white",
-            width=300
-        )
+        content="Ingresar", 
+        bgcolor="pink200", 
+        color="white", 
+        width=300
+    )
 
+    page.add(
+        icono,
+        titulo,
+        nombre,
+        contrasena,
+        boton
+    )
 
 ft.app(target=main)
